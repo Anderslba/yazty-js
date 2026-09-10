c1ccf9aae13585f1bf431b11acfb182d7187773e
Andreas <anskipper1@gmail.com>
2026-09-10 17:58:50 +0200

    Changes

--- dom.js
+++ dom.js
@@ -1,4 +1,10 @@
-import yatzy from 'yatzy.js'
+import { startGame } from "./yatzy.js";
 
-const btnRollDie = document.querySelector('#roll-button')
-const btnReset = document.querySelector('reset-button')
+const btnRollDie = document.querySelector('#roll-button');
+const btnReset = document.querySelector('reset-button');
+const diceElement = document.querySelector('.die');
+
+
+
+const game = startGame();
+
