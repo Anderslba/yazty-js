6dc6750fe59265dffea79e899dac272fcfb38240
Anderslba <anderslba@gmail.com>
2026-09-10 15:25:55 +0200

    lidt dom lavet

--- dom.js
+++ dom.js
@@ -1,4 +1,13 @@
-import yatzy from 'yatzy.js'
-
 const btnRollDie = document.querySelector('#roll-button')
 const btnReset = document.querySelector('reset-button')
+const dice = document.querySelectorAll('.dice > img')
+
+const game = startGame()
+
+btnRollDie.addEventListener('click', () => {
+    game.rollDice()
+
+    for (let i in dice) {
+        dice[i].src = `Terninger/${game.dice[i].value} terning.png`
+    }
+})
